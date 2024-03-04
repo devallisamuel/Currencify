b2c89e2978c12794afa2e0df0c2ec57ea6a97211
Alli Samuel <samuel.alli@wemabank.com>
2024-03-04 14:23:21 +0100

    converted code to functions

--- index.ts
+++ index.ts
@@ -1,32 +1,43 @@
 import { store } from "./store";
 import { Currency } from "./type";
 
- class Currencify {
-  invalid: boolean = false;
-  prefix: string = "";
-  suffix: string = "";
-  amount: string = "";
-  currency: Currency = {
-    start: "",
-    end: "",
-    country: "",
-  };
 
-  regex = /^[A-Za-z !@#$%^&*()_+={}[\]:;<>,?~\\/-]+$/;
+  const Currencify = (country:string) => {
+    // @ts-ignore
+    this.invalid = false;
+    // @ts-ignore
+    this.prefix = "";
+    // @ts-ignore
+    this.suffix = "";
+    // @ts-ignore
+    this.amount = "";
+    // @ts-ignore
+    this.regex = /^[A-Za-z !@#$%^&*()_+={}[\]:;<>,?~\\/-]+$/;
+    // @ts-ignore
+    this.country = country;
+    // @ts-ignore
+    this.currency = {
+      start: "",
+      end: "",
+      country: "",
+    };
 
-  constructor(private country: string) {
     store.forEach((val) => {
       if (
+        // @ts-ignore
         val.country.toLocaleLowerCase() === this.country.toLocaleLowerCase()
       ) {
+        // @ts-ignore
         this.currency = val;
       }
     });
   }
 
-  formatCurrency(amount:string) {
+  Currencify.prototype.formatCurrency = function (
+    amount: string
+  ) {
     try {
-        this.amount = amount;
+      this.amount = amount;
       if (!this.currency?.country || this.regex.test(this.amount)) {
         this.invalid = true;
         throw Error("Invalid Input Format");
@@ -42,8 +53,52 @@ import { Currency } from "./type";
       console.log(err);
       return err;
     }
-  }
-}
+  };
+
+
+//  class Currencify {
+//   invalid: boolean = false;
+//   prefix: string = "";
+//   suffix: string = "";
+//   amount: string = "";
+//   currency: Currency = {
+//     start: "",
+//     end: "",
+//     country: "",
+//   };
+
+//   regex = /^[A-Za-z !@#$%^&*()_+={}[\]:;<>,?~\\/-]+$/;
+
+//   constructor(private country: string) {
+//     store.forEach((val) => {
+//       if (
+//         val.country.toLocaleLowerCase() === this.country.toLocaleLowerCase()
+//       ) {
+//         this.currency = val;
+//       }
+//     });
+//   }
+
+//   formatCurrency(amount:string) {
+//     try {
+//         this.amount = amount;
+//       if (!this.currency?.country || this.regex.test(this.amount)) {
+//         this.invalid = true;
+//         throw Error("Invalid Input Format");
+//       } else {
+//         const regexp = /\B(?=(\d{3})+(?!\d))/g;
+//         this.amount = this.amount.replace(regexp, ",");
+//         this.prefix = this.currency?.start ?? "";
+//         this.suffix = this.currency?.end ?? "";
+//         return this.prefix + this.amount + this.suffix;
+//       }
+//     } catch (error) {
+//       let err = "Currencify " + error;
+//       console.log(err);
+//       return err;
+//     }
+//   }
+// }
 
 
 export default Currencify;
